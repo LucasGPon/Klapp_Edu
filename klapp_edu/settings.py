186bd2c215a2396361ba56fd93f9cecdb7a6385f
Felipe_Lucas8 <felipe.silva@fit-tecnologia.org.br>
2023-04-02 22:22:31 -0300

    Landing page e página de login

--- klapp_edu/settings.py
+++ klapp_edu/settings.py
@@ -31,9 +31,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'klapp_edu',
     'home',
     'login',
     'alunos',
+    'bootstrap4'
 ]
    
  
@@ -115,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
